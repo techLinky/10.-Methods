18be51da95e86300b9e0d2cd4870d7f501a318b8
techLinky <gcebile.sne@gmail.com>
2023-09-13 18:18:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //call for the sayHi method
+            sayHi();
+            //call for the AddNumbers method
+            AddNumbers();
+           
+            //freeze console
+            Console.ReadLine();
+
+        }
+
+        static void sayHi()
+        {
+
+            Console.WriteLine("Your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+
+        }
+
+        static void AddNumbers()
+        {
+
+            Console.WriteLine("Enter first number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+          
+
+
+        }
+
+
+
+    }
+}
